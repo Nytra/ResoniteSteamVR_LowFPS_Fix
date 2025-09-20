becf845ebc750d371718fb7295100a3b54f4c0b1
Nytra <14206961+Nytra@users.noreply.github.com>
2025-09-20 13:34:20 +0100

    Add files

---
+++ SteamVR_LowFPS_Fix/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Renderite.Unity;
+using Valve.VR;
+
+namespace SteamVR_LowFPS_Fix;
+
+[BepInPlugin("Nytra.SteamVR_LowFPS_Fix", "SteamVR Low FPS Fix", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    public static ManualLogSource? Log;
+
+	void Awake()
+	{
+		Log = base.Logger;
+
+		var harmony = new Harmony("Nytra.SteamVR_LowFPS_Fix");
+		harmony.PatchAll();
+	}
+}
+
+[HarmonyPatch(typeof(RenderingManager), "UpdateVR_Active")]
+class Patch
+{
+	static bool Prefix(bool vrActive)
+	{
+		SteamVR.instance?.compositor.SuspendRendering(!vrActive);
+		return true;
+	}
+}
